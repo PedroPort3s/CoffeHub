048a0cc7f8895419587ceafb54c1391d36096839
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2020-11-09 18:50:43 -0300

    add. tabela funcionaro

--- src/views/controllers/funcionario/PesquisaFuncionarioController.java
+++ src/views/controllers/funcionario/PesquisaFuncionarioController.java
@@ -2,6 +2,7 @@ package views.controllers.funcionario;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -9,9 +10,12 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import com.sun.deploy.uitoolkit.impl.fx.ui.FXConsole;
 
 import dao.FuncionarioDAO;
 import entitys.Funcionario;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +23,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -28,50 +35,167 @@ import views.controllers.HomeController;
 public class PesquisaFuncionarioController {
 
 	private static Stage pesquisaFuncionario;
-	
+
 	private FuncionarioDAO dao = new FuncionarioDAO();
-	
-    @FXML
-    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	// TABELA:
 
-    @FXML
-    private JFXTextField txtCodPesquisa;
+	@FXML
+	private TableView<Funcionario> tableView;
 
-    @FXML
-    private JFXTextField txtNomePesquisa;
+	@FXML
+	private TableColumn<Funcionario, Integer> cCod;
 
-    @FXML
-    private JFXButton btnPesquisar;
+	@FXML
+	private TableColumn<Funcionario, String> cDoc;
 
-    @FXML
-    private JFXButton btnCadFuncionario;
+	@FXML
+	private TableColumn<Funcionario, String> cFone;
 
-    @FXML
-    private JFXButton btnVoltar;
+	@FXML
+	private TableColumn<Funcionario, String> cNome;
 
-    @FXML
-    private JFXListView<Funcionario> lvFuncionarios;
+	@FXML
+	private TableColumn<Funcionario, String> cEndereco;
 
-    @FXML
-    void lvFuncionario_MouseClicked(MouseEvent event) {
+	@FXML
+	private TableColumn<Funcionario, String> cEmail;
+
+	@FXML
+	private TableColumn<Funcionario, Double> cSalario;
+
+	@FXML
+	private TableColumn<Funcionario, LocalDate> cDataContratacao;
+
+	@FXML
+	private TableColumn<Funcionario, LocalDate> cDataDemissao;
+
+	@FXML
+	private TableColumn<Funcionario, String> cSenha;
+
+	@FXML
+	private TableColumn<Funcionario, Integer> cAcesso;
+	// -------------------------------------------------------------------
+
+	// LISTAR FUNCIONARIOS
+
+	private void listarFuncionarios() {
+		ObservableList<Funcionario> listFunc = FXCollections.observableArrayList();
+
+		// parece que as listas funcionam de uma fora diferente do ArrayList(), ela utiliza "ObservableList()"..
+
+
+		try {
+			
+			listFunc = (ObservableList<Funcionario>) dao.listar();
+
+			cCod.setCellValueFactory(new PropertyValueFactory<Funcionario, Integer>("cod"));
+			cDoc.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("documento"));
+			cFone.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("telefone"));
+			cNome.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("nome"));
+			cEndereco.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("endereco"));
+			cEmail.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("email"));
+			cSalario.setCellValueFactory(new PropertyValueFactory<Funcionario, Double>("salario"));
+			cDataContratacao.setCellValueFactory(new PropertyValueFactory<Funcionario, LocalDate>("data_contratacao"));
+			cDataDemissao.setCellValueFactory(new PropertyValueFactory<Funcionario, LocalDate>("data_demissao"));
+			cSenha.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("senha_funcionario"));
+			cAcesso.setCellValueFactory(new PropertyValueFactory<Funcionario, Integer>("cod_acesso"));
+
+			tableView.setItems(listFunc);
+
+		} catch (Exception e) {
+			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
+			alert.showAndWait();
+		}
+
+//    	List<Funcionario> lstFuncionarios;    	
+//    	lvFuncionarios.getItems().clear();
+//    	
+//		try {
+//			lstFuncionarios = dao.listar();
+//			if (lstFuncionarios != null) {
+//				if (txtCodPesquisa != null && conferirNumero(txtCodPesquisa.getText(), "Insira um número")) {
+//					List<Funcionario> listaCod = new ArrayList<Funcionario>();
+//					for (Funcionario f : lstFuncionarios) {
+//						if ((f.getCod() == Integer.parseInt(txtCodPesquisa.getText()))) {
+//							listaCod.add(f);
+//						}
+//					}
+//					lstFuncionarios = listaCod;
+//				}
+//				if (txtNomePesquisa != null) {
+//					List<Funcionario> listaNome = new ArrayList<Funcionario>();
+//					for (Funcionario f : lstFuncionarios) {
+//						if (f.getNome().matches(".*" + txtNomePesquisa.getText() + ".*")) {
+//							listaNome.add(f);
+//						}
+//					}
+//					lstFuncionarios = listaNome;
+//				}
+//				lstFuncionarios.forEach(f -> lvFuncionarios.getItems().add(f));
+//			}
+//		} catch (Exception e) {
+//			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
+//			alert.showAndWait();
+//		}
+//    }
+//
+//    private Boolean conferirNumero(String texto, String msg) {
+//    	try {
+//    		if(!texto.equals("")) {
+//    			Integer.parseInt(texto);
+//    		} else if(texto.equals("")) {
+//    			return false;
+//    		}
+//		} catch (Exception e) {
+//			throw new NumberFormatException(msg);
+//		}
+//    	return true;
+	}
+
+	// -------------------------------------------------------------------
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	private JFXTextField txtCodPesquisa;
+
+	@FXML
+	private JFXTextField txtNomePesquisa;
+
+	@FXML
+	private JFXButton btnPesquisar;
+
+	@FXML
+	private JFXButton btnCadFuncionario;
+
+	@FXML
+	private JFXButton btnVoltar;
+
+	@FXML
+	private JFXListView<Funcionario> lvFuncionarios;
+
+	@FXML
+	void lvFuncionario_MouseClicked(MouseEvent event) {
 		try {
 			Funcionario funcionario = lvFuncionarios.getSelectionModel().getSelectedItem();
 			if (funcionario != null) {
-					CadFuncionarioController.funcionarioStatic = funcionario;
-					fechar();
-					new CadFuncionarioController().getCadFuncionario().show();
+				CadFuncionarioController.funcionarioStatic = funcionario;
+				fechar();
+				new CadFuncionarioController().getCadFuncionario().show();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
 			alert.showAndWait();
 		}
-    }
-    
-    public Stage getPesquisaFuncionario() {
+	}
+
+	public Stage getPesquisaFuncionario() {
 		if (pesquisaFuncionario == null) {
 			try {
 				Stage primaryStage = new Stage();
@@ -90,90 +214,89 @@ public class PesquisaFuncionarioController {
 		}
 		return pesquisaFuncionario;
 	}
-    
-    private void fechar() {
-    	pesquisaFuncionario.close();
-    	pesquisaFuncionario = null;
-    }
-    
-    @FXML
-    void btnCadFuncionario_Action(ActionEvent event) {
-    	fechar();
+
+	private void fechar() {
+		pesquisaFuncionario.close();
+		pesquisaFuncionario = null;
+	}
+
+	@FXML
+	void btnCadFuncionario_Action(ActionEvent event) {
+		fechar();
 		new CadFuncionarioController().getCadFuncionario().show();
-    }
+	}
 
-    @FXML
-    void btnPesquisar_Action(ActionEvent event) {
-    	listarFuncionarios();
-    }
-    
-    private void listarFuncionarios() {
-    	List<Funcionario> lstFuncionarios;
-    	lvFuncionarios.getItems().clear();
-    	
+	@FXML
+	void btnPesquisar_Action(ActionEvent event) {
+		listarFuncionarios();
+	}
+
+//    private void listarFuncionarios() {
+//    	List<Funcionario> lstFuncionarios;
+//    	lvFuncionarios.getItems().clear();
+//    	
+//		try {
+//			lstFuncionarios = dao.listar();
+//			if (lstFuncionarios != null) {
+//				if (txtCodPesquisa != null && conferirNumero(txtCodPesquisa.getText(), "Insira um número")) {
+//					List<Funcionario> listaCod = new ArrayList<Funcionario>();
+//					for (Funcionario f : lstFuncionarios) {
+//						if ((f.getCod() == Integer.parseInt(txtCodPesquisa.getText()))) {
+//							listaCod.add(f);
+//						}
+//					}
+//					lstFuncionarios = listaCod;
+//				}
+//				if (txtNomePesquisa != null) {
+//					List<Funcionario> listaNome = new ArrayList<Funcionario>();
+//					for (Funcionario f : lstFuncionarios) {
+//						if (f.getNome().matches(".*" + txtNomePesquisa.getText() + ".*")) {
+//							listaNome.add(f);
+//						}
+//					}
+//					lstFuncionarios = listaNome;
+//				}
+//				lstFuncionarios.forEach(f -> lvFuncionarios.getItems().add(f));
+//			}
+//		} catch (Exception e) {
+//			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
+//			alert.showAndWait();
+//		}
+//    }
+//
+//    private Boolean conferirNumero(String texto, String msg) {
+//    	try {
+//    		if(!texto.equals("")) {
+//    			Integer.parseInt(texto);
+//    		} else if(texto.equals("")) {
+//    			return false;
+//    		}
+//		} catch (Exception e) {
+//			throw new NumberFormatException(msg);
+//		}
+//    	return true;
+//    }
+
+	@FXML
+	void btnVoltar_Action(ActionEvent event) {
 		try {
-			lstFuncionarios = dao.listar();
-			if (lstFuncionarios != null) {
-				if (txtCodPesquisa != null && conferirNumero(txtCodPesquisa.getText(), "Insira um número")) {
-					List<Funcionario> listaCod = new ArrayList<Funcionario>();
-					for (Funcionario f : lstFuncionarios) {
-						if ((f.getCod() == Integer.parseInt(txtCodPesquisa.getText()))) {
-							listaCod.add(f);
-						}
-					}
-					lstFuncionarios = listaCod;
-				}
-				if (txtNomePesquisa != null) {
-					List<Funcionario> listaNome = new ArrayList<Funcionario>();
-					for (Funcionario f : lstFuncionarios) {
-						if (f.getNome().matches(".*" + txtNomePesquisa.getText() + ".*")) {
-							listaNome.add(f);
-						}
-					}
-					lstFuncionarios = listaNome;
-				}
-				lstFuncionarios.forEach(f -> lvFuncionarios.getItems().add(f));
-			}
-		} catch (Exception e) {
-			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
-			alert.showAndWait();
-		}
-    }
-
-    private Boolean conferirNumero(String texto, String msg) {
-    	try {
-    		if(!texto.equals("")) {
-    			Integer.parseInt(texto);
-    		} else if(texto.equals("")) {
-    			return false;
-    		}
-		} catch (Exception e) {
-			throw new NumberFormatException(msg);
-		}
-    	return true;
-    }
-
-    
-    @FXML
-    void btnVoltar_Action(ActionEvent event) {
-		try {
-	    	fechar();
+			fechar();
 			new HomeController().getHome().show();
 		} catch (Exception e) {
 			Alert alert = new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
 			alert.showAndWait();
 		}
-    }
+	}
 
-    @FXML
-    void initialize() {
-        assert txtCodPesquisa != null : "fx:id=\"txtCodPesquisa\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
-        assert txtNomePesquisa != null : "fx:id=\"txtNomePesquisa\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
-        assert btnPesquisar != null : "fx:id=\"btnPesquisar\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
-        assert btnCadFuncionario != null : "fx:id=\"btnCadFuncionario\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
-        assert btnVoltar != null : "fx:id=\"btnVoltar\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
-        assert lvFuncionarios != null : "fx:id=\"lvFuncionarios\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+	@FXML
+	void initialize() {
+		assert txtCodPesquisa != null : "fx:id=\"txtCodPesquisa\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+		assert txtNomePesquisa != null : "fx:id=\"txtNomePesquisa\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+		assert btnPesquisar != null : "fx:id=\"btnPesquisar\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+		assert btnCadFuncionario != null : "fx:id=\"btnCadFuncionario\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+		assert btnVoltar != null : "fx:id=\"btnVoltar\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
+		assert lvFuncionarios != null : "fx:id=\"lvFuncionarios\" was not injected: check your FXML file 'PesquisaFuncionario.fxml'.";
 
-        listarFuncionarios();
-    }
+		listarFuncionarios();
+	}
 }
